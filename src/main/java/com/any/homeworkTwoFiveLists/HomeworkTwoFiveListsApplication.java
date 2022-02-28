430e8d62113905efe0ff7f50e60f7b1f475a8129
dispersion <anyta1901@gmail.com>
2022-02-28 18:18:22 +0300

    Initial commit

---
+++ src/main/java/com/any/homeworkTwoFiveLists/HomeworkTwoFiveListsApplication.java
@@ -0,0 +1,13 @@
+package com.any.homeworkTwoFiveLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTwoFiveListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkTwoFiveListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/any/homeworkTwoFiveLists/HomeworkTwoFiveListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.any.homeworkTwoFiveLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkTwoFiveListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
